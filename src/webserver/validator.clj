3f28a400e4aee43ccb6a8849a0b005f49668c0a1
hkgumbs <h.kofigumbs@gmail.com>
2015-08-10 14:20:13 -0500

    Add header field validation

--- src/webserver/validator.clj
+++ src/webserver/validator.clj
@@ -1,13 +1,23 @@
 (ns webserver.validator)
 
-(defn parse [request]
+(defn parse-request-line [request-line]
   (let [regex #"(^[A-Z]+) (.+) (HTTP/\d\.\d)$"
-        [_ method uri version] (first (re-seq regex request))]
+        [_ method uri version] (first (re-seq regex request-line))]
     {:method method
      :uri uri
-     :version version
-     }))
+     :version version}))
+
+(defn parse-header-fields
+  ([headers] (parse-header-fields {} (.split headers "\r\n")))
+  ([acc [header & more]]
+   (if (nil? header)
+     acc
+     (let [regex #"^([\w\-]+): ([^\r\n]+)$"
+           [_ field-name field-value] (first (re-seq regex header))
+           acc (assoc acc (keyword field-name) field-value) ]
+       (recur acc more)))))
 
 (defn valid? [parsed-request]
-  (every? (comp not nil?) (vals parsed-request))
-  )
+  (every? (comp not nil?) (vals parsed-request)))
+
+
--- test/webserver/validator_test.clj
+++ test/webserver/validator_test.clj
@@ -2,32 +2,62 @@
   (:require [clojure.test :refer :all]
             [webserver.validator :refer :all]))
 
-(deftest parse-request-line
-  (def example1 "GET http://www.example.com HTTP/1.1")
-  (def example2 "POST http://8thlight.com HTTP/1.0")
-
+(deftest validate-request-line
   (testing "Correctly formed request lines"
-    (is (let [table (parse example1)]
-          (and
-            (= (:method table) "GET")
-            (= (:uri table) "http://www.example.com")
-            (= (:version table) "HTTP/1.1")
-            )))
-    (is (let [table (parse example2)]
-          (and
-            (= (:method table) "POST")
-            (= (:uri table) "http://8thlight.com")
-            (= (:version table) "HTTP/1.0")
-            )))
+    (def example1 "GET http://www.example.com HTTP/1.1")
+    (def example2 "POST http://8thlight.com HTTP/1.0")
+    (def table1 (parse-request-line example1))
+    (def table2 (parse-request-line example2))
+
+    (is (= (:method table1) "GET"))
+    (is (= (:uri table1) "http://www.example.com"))
+    (is (= (:version table1) "HTTP/1.1"))
+    (is (= (:method table2) "POST"))
+    (is (= (:uri table2) "http://8thlight.com"))
+    (is (= (:version table2) "HTTP/1.0"))
+    (is (valid? (parse-request-line example1)))
     )
 
   (testing "Malformed request line"
-    (is (valid? (parse example1)))
-    (is (not (valid? (parse "GET http://www.example.com"))))
-    (is (not (valid? (parse "GET http://www.example.com foo23"))))
-    (is (not (valid? (parse "get http://www.example.com HTTP/1.1"))))
-    (is (not (valid? (parse "GET http://www.example.com HTTP/1.1 asdf"))))
-    (is (not (valid? (parse "hello GET http://www.example.com HTTP/1.1"))))
+    (is (not (valid? (parse-request-line "GET http://www.example.com"))))
+    (is (not (valid? (parse-request-line
+                 "GET http://www.example.com foo23"))))
+    (is (not (valid? (parse-request-line
+                 "get http://www.example.com HTTP/1.1"))))
+    (is (not (valid? (parse-request-line
+                 "GET http://www.example.com HTTP/1.1 asdf"))))
+    (is (not (valid? (parse-request-line
+                 "hello GET http://www.example.com HTTP/1.1"))))
+    )
+
+  )
+
+(deftest validate-header-fields
+  (def headers ["User-Agent: curl/7.16.3 libcurl/7.16.3"
+                "Host: www.example.com"
+                "Accept-Language: en, mi"
+                "Date: Mon, 10 Aug 2015 12:32:56 GMT"
+                "Content-Type: text/plain"
+                "Content-MD5: Q2hlY2sgSW50ZWdyaXR5IQ=="
+                ])
+  (def example (apply str (interpose "\r\n" headers)))
+  (def table (parse-header-fields example))
+
+  (testing "Correctly formed header fields"
+    (is (valid? table))
+    (is (= (:User-Agent table) "curl/7.16.3 libcurl/7.16.3"))
+    (is (= (:Host table) "www.example.com"))
+    (is (= (:Accept-Language table) "en, mi"))
+    (is (= (:Date table) "Mon, 10 Aug 2015 12:32:56 GMT"))
+    (is (= (:Content-Type table) "text/plain"))
+    (is (= (:Content-MD5 table) "Q2hlY2sgSW50ZWdyaXR5IQ=="))
+    )
+
+  (testing "Malformed header fields"
+    (is (not (valid? (parse-header-fields
+                       (str example "\r\n foo :bar")))))
+    (is (not (valid? (parse-header-fields
+                       "Hello World: cannot have space in name"))))
     )
   )
 
