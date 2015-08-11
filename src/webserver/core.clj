4959ac6b29196d22edee1108ea12246658c5da8e
hkgumbs <h.kofigumbs@gmail.com>
2015-08-10 19:40:57 -0500

    Add command-line-argument parsing

--- src/webserver/core.clj
+++ src/webserver/core.clj
@@ -1,6 +1,15 @@
 (ns webserver.core
   (:gen-class))
 
+(def PORT "5000")
+(def PUBLIC_DIR
+  "/Users/hkgumbs/Workspaces/8thLight/webserver-clojure/cob_spec/public")
+
+(defn parse-args [args]
+  (let [m (reduce #(assoc % (first %2) (second %2)) {} (partition 2 args))]
+    {:port (m "-p" PORT) :dir (m "-d" PUBLIC_DIR)}
+    ))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/webserver/core_test.clj
+++ test/webserver/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [webserver.core :refer :all]))
 
-(deftest a-test
-  (testing "Default test"
-    (is (= 1 1))))
+(deftest parse-args-test
+  (testing "Port option"
+    (is (= "80" (:port (parse-args ["-p" "80"]))))
+    (is (= "8080" (:port (parse-args ["-p" "8080"]))))
+    (is (= PORT (:port (parse-args []))))
+    )
+
+  (testing "Public dir option"
+    (is (= "/some/random/path" (:dir (parse-args ["-d" "/some/random/path"]))))
+    (is (= "/" (:dir (parse-args ["-d" "/"]))))
+    (is (= PUBLIC_DIR (:dir (parse-args []))))
+    )
+
+  (testing "Both options"
+    (is (= {:port "80" :dir "/tmp"} (parse-args ["-p" "80" "-d" "/tmp"])))
+    (is (= {:port PORT :dir PUBLIC_DIR} (parse-args [])))
+    )
+  )
