9dc55e423b5029edaf749e79079a63ed38271061
hkgumbs <h.kofigumbs@gmail.com>
2015-08-22 15:32:52 -0500

    Refactor upload tests
    - Move test cases into loop to remvoe redundency

--- test/cob_app/add_test.clj
+++ test/cob_app/add_test.clj
@@ -4,54 +4,43 @@
             [cob-app.core :refer [initialize handle]]
             [webserver.mock-socket]))
 
-(describe "Both PUT and POST requests"
-  (before-all
-    (.mkdir (java.io.File. "./tmp"))
-    (initialize ["-d" "./tmp"]))
+(describe "Upload requests"
 
-  (after
-    (clojure.java.io/delete-file "./tmp/foo.bar"))
+    (before-all
+      (.mkdir (java.io.File. "./tmp"))
+      (initialize ["-d" "./tmp"]))
 
-  (after-all
-    (clojure.java.io/delete-file "./tmp"))
+    (after
+       (clojure.java.io/delete-file "./tmp/foo.bar"))
 
-  (with-all body "This is a testing content for the text file foo.bar")
+    (after-all
+       (clojure.java.io/delete-file "./tmp"))
 
-  (it "stores basic text file (PUT)"
-    (should=
-      "HTTP/1.1 200 OK\r\n\r\n"
-      (webserver.mock-socket/connect
-        handle
-        {:method "PUT"
-         :uri "/foo.bar"
-         :version "HTTP/1.1"
-         :Content-Length "51"
-         :Content-Type "text/plain"}
-        @body))
-    (should (.exists (java.io.File. "./tmp/foo.bar"))))
+    (with-all body "This is a testing content for the text file foo.bar")
 
- (it "stores basic text file (POST)"
-    (should=
-      "HTTP/1.1 200 OK\r\n\r\n"
-      (webserver.mock-socket/connect
-        handle
-        {:method "POST"
-        :uri "/foo.bar"
-        :version "HTTP/1.1"
-        :Content-Length "51"
-        :Content-Type "text/plain"}
-        @body))
-    (should (.exists (java.io.File. "./tmp/foo.bar"))))
+    (for [method ["PUT" "POST"]]
+      (it (format "stores basic text file (%s)" method)
+        (should=
+          "HTTP/1.1 200 OK\r\n\r\n"
+          (webserver.mock-socket/connect
+            handle
+            {:method method
+             :uri "/foo.bar"
+             :version "HTTP/1.1"
+             :Content-Length "51"
+             :Content-Type "text/plain"}
+            @body))
+        (should (.exists (java.io.File. "./tmp/foo.bar")))))
 
- (it "Doesn't fail on empty Content-Length and body (POST)"
-    (should=
-      "HTTP/1.1 200 OK\r\n\r\n"
-      (webserver.mock-socket/connect
-        handle
-        {:method "POST"
-         :uri "/foo.bar"
-         :version "HTTP/1.1"}
-        @body))
-    (should (.exists (java.io.File. "./tmp/foo.bar"))))
-  )
+    (for [method ["PUT" "POST"]]
+      (it (format "doesn't fail on empty Content-Length and body (%s)" method)
+        (should=
+          "HTTP/1.1 200 OK\r\n\r\n"
+          (webserver.mock-socket/connect
+            handle
+            {:method method
+             :uri "/foo.bar"
+             :version "HTTP/1.1"}
+            @body))
+        (should (.exists (java.io.File. "./tmp/foo.bar"))))))
 
