9d64a7a0b7a16cc1705888f45b33e7bde2bdd11f
hkgumbs <h.kofigumbs@gmail.com>
2015-08-25 11:47:04 -0500

    Fix pandect dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.codec "0.1.0"]]
+                 [org.clojure/data.codec "0.1.0"]
+                 [pandect "0.5.3"]]
   :main ^:skip-aot webserver.core
   :target-path "target/%s"
   :profiles {:dev
