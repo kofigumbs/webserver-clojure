e735c6471017ea29b1f8ce5e0586311ce1fc3b15
hkgumbs <h.kofigumbs@gmail.com>
2015-08-25 11:42:47 -0500

    Fix codec dependency

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.codec "0.1.0"]]
   :main ^:skip-aot webserver.core
   :target-path "target/%s"
   :profiles {:dev
-             {:dependencies [[speclj "3.3.0"]
-                             [org.clojure/data.codec "0.1.0"]]}
+             {:dependencies [[speclj "3.3.0"]]}
              :uberjar {:aot :all}}
   :plugins [[speclj "3.3.0"]])
